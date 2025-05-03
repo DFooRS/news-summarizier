c431e1cf802f15006e5c07483ba4b90c77e406cf
DFooRS <dima.lutsenko03@gmail.com>
2025-05-03 17:35:21 +0300

    initial backeand

---
+++ backend/app/core/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app.config import settings
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql+asyncpg://"
+    f"{settings.DB_USER}:{settings.DB_PASSWORD}@"
+    f"{settings.DB_HOST}:{settings.DB_PORT}/"
+    f"{settings.DB_NAME}"
+)
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+Base = declarative_base()
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
